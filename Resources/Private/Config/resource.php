1722e81f2b9f6f71e6506f1624c8aaf582e19dc5
Aimeos <aimeos@aimeos.org>
2015-03-13 12:14:32 +0100

    Support sockets for MySQL too

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -5,6 +5,7 @@ return array(
 		'adapter' => 'mysql',
 		'host' => $GLOBALS['TYPO3_CONF_VARS']['DB']['host'],
 		'port' => $GLOBALS['TYPO3_CONF_VARS']['DB']['port'],
+		'socket' => $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'],
 		'database' => $GLOBALS['TYPO3_CONF_VARS']['DB']['database'],
 		'username' => $GLOBALS['TYPO3_CONF_VARS']['DB']['username'],
 		'password' => $GLOBALS['TYPO3_CONF_VARS']['DB']['password'],
