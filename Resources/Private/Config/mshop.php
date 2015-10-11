2673c8ff7eb13ebbb8295ea2b90032e5ea313352
Aimeos <aimeos@aimeos.org>
2015-10-11 18:22:50 +0200

    Use MySQL implementation of index managers

--- Resources/Private/Config/mshop.php
+++ Resources/Private/Config/mshop.php
@@ -4,6 +4,15 @@ return array(
 	'index' => array(
 		'manager' => array(
 			'name' => 'MySQL',
+			'attribute' => array(
+				'name' => 'MySQL',
+			),
+			'catalog' => array(
+				'name' => 'MySQL',
+			),
+			'price' => array(
+				'name' => 'MySQL',
+			),
 			'text' => array(
 				'name' => 'MySQL',
 			),
