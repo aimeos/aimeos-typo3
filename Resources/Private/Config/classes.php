8ec870f935654918c1c13c627c0c813ccfe2bf02
Aimeos <aimeos@aimeos.org>
2015-10-16 14:21:00 +0200

    Use MySQL specific catalog index implementations

--- Resources/Private/Config/classes.php
+++ Resources/Private/Config/classes.php
@@ -1,19 +1,28 @@
 <?php
 
 return array(
+	'customer' => array(
+		'manager' => array(
+			'name' => 'Typo3',
+		)
+	),
 	'catalog' => array(
 		'manager' => array(
 			'index' => array(
 				'name' => 'MySQL',
+				'attribute' => array(
+					'name' => 'MySQL',
+				),
+				'catalog' => array(
+					'name' => 'MySQL',
+				),
+				'price' => array(
+					'name' => 'MySQL',
+				),
 				'text' => array(
 					'name' => 'MySQL',
 				),
 			),
 		),
-	),
-	'customer' => array(
-		'manager' => array(
-			'name' => 'Typo3',
-		)
 	),
 );
