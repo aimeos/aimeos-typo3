540a37afbe0f3a2c73ef25d1197a00f8b0b45641
Aimeos <aimeos@aimeos.org>
2016-10-08 16:44:32 +0200

    Update from core

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -3,12 +3,12 @@
 return array(
 	'db' => array(
 		'adapter' => 'mysql',
-		'host' => $GLOBALS['TYPO3_CONF_VARS']['DB']['host'],
-		'port' => $GLOBALS['TYPO3_CONF_VARS']['DB']['port'],
-		'socket' => $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'],
-		'database' => $GLOBALS['TYPO3_CONF_VARS']['DB']['database'],
-		'username' => $GLOBALS['TYPO3_CONF_VARS']['DB']['username'],
-		'password' => $GLOBALS['TYPO3_CONF_VARS']['DB']['password'],
+		'host' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['host'] ),
+		'port' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['port'] ),
+		'socket' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['socket'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['socket'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'] ),
+		'database' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] ),
+		'username' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['username'] ),
+		'password' => ( isset( $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] ) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] : $GLOBALS['TYPO3_CONF_VARS']['DB']['password'] ),
 		'stmt' => array(
 			"SET NAMES 'utf8'",
 			"SET SESSION sql_mode='ANSI'",
