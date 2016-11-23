c54526a16c541b4c26b756910fcbe8d5c3791573
Aimeos <aimeos@aimeos.org>
2016-11-23 17:25:46 +0100

    Exclude .git directories

--- .deploy.sh
+++ .deploy.sh
@@ -2,17 +2,19 @@
 
 set -ev
 
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 DATE=`date -u +%Y-%m-%d`
 
-if test `git rev-parse --abbrev-ref HEAD` != 'master'; then
+if test $BRANCH -ne 'master'; then
 	VERSION="`git describe --abbrev=0 --tags --always`-dev"
 else
 	VERSION=`date -u +%y.%m.%d-dev`
 fi
 
+echo "$BRANCH, $VERSION"
 
 cat .deploy.json | sed "s/###DATE###/$DATE/" > .deploy.json.new
 cat .deploy.json.new | sed "s/###VERSION###/$VERSION/" > .deploy.json
 
 
-zip -r ../aimeos_$VERSION.zip * -x .tx -x .git* -x composer.* -x .deploy.* -x .travis.yml
+zip -r ../aimeos_$VERSION.zip * -x .tx -x *.git* -x composer.* -x .deploy.* -x .travis.yml
