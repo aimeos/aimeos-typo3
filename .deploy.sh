094d23066e7b40af2663c0e5340f729da486d6c0
Aimeos <aimeos@aimeos.org>
2016-11-23 17:57:34 +0100

    Create packages automatically

---
+++ .deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ev
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+DATE=`date -u +%Y-%m-%d`
+
+if test $BRANCH != 'master' && test $BRANCH != 'HEAD'; then
+	VERSION="`git describe --abbrev=0 --tags --always`-dev"
+else
+	VERSION=`date -u +%y.%m.%d-dev`
+fi
+
+echo "$BRANCH, $VERSION"
+
+cat .deploy.json | sed "s/###DATE###/$DATE/g" > .deploy.json.new
+cat .deploy.json.new | sed "s/###VERSION###/$VERSION/g" > .deploy.json
+
+
+zip -r ../aimeos_$VERSION.zip * -x .tx -x *.git* -x composer.* -x .deploy.* -x .travis.yml
