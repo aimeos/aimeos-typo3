11ca3cc5721819b962b060dab12366da2f995c3e
Gilbertsoft <it@gilbertsoft.ch>
2017-02-13 12:14:34 +0100

    Changed language and currency parameter names to defaults in Aimeos\Aimeos\Base\Local

---
+++ Resources/Private/Config/client.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	'html' => array(
+		'locale' => array(
+			'select' => array(
+
+				// Language related settings
+				'language' => array(
+					// Name of the parameter that contains the language ID value
+					'param-name' => 'loc_language',
+				),
+
+				// Currency related settings
+				'currency' => array(
+					// Name of the parameter that contains the currency ID value
+					'param-name' => 'loc_currency',
+				),
+			),
+		),
+	),
+);
