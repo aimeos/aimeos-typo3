cfffe363ff50eb3a2f2bbc68c656ef6e8c4c490d
Aimeos <aimeos@aimeos.org>
2017-03-05 00:22:33 +0100

    Required changes for TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,11 +25,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4.0-7.99.99',
-			'typo3' => '6.2.0-7.99.99',
-			'fluid' => '6.2.0-7.99.99',
-			'extbase' => '6.2.0-7.99.99',
-			'scheduler' => '6.2.0-7.99.99',
-			'static_info_tables' => '6.0.0-7.99.99',
+			'typo3' => '6.2.0-8.99.99',
+			'fluid' => '6.2.0-8.99.99',
+			'extbase' => '6.2.0-8.99.99',
+			'scheduler' => '6.2.0-8.99.99',
+			'static_info_tables' => '6.0.0-6.99.99',
 		),
 		'conflicts' => array(
 		),
