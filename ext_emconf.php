c16d8ddde0d52f6453520d38317673c38cd03717
Aimeos <aimeos@aimeos.org>
2017-03-14 12:13:36 +0100

    Removed unnecessary extbase/fluid dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,8 +26,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.4.0-8.99.99',
 			'typo3' => '6.2.0-8.99.99',
-			'fluid' => '6.2.0-8.99.99',
-			'extbase' => '6.2.0-8.99.99',
 			'scheduler' => '6.2.0-8.99.99',
 			'static_info_tables' => '6.0.0-6.99.99',
 		),
