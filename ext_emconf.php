5e2af355bfe62640bad4d052c08c8ddc81d04eb0
Aimeos <aimeos@aimeos.org>
2018-01-15 17:38:21 +0100

    Support for TYPO3 7.6 and 8.7 only

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,14 +25,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4.0-8.99.99',
-			'typo3' => '6.2.0-8.99.99',
-			'scheduler' => '6.2.0-8.99.99',
+			'typo3' => '7.6.0-8.99.99',
+			'scheduler' => '7.6.0-8.99.99',
 			'static_info_tables' => '6.0.0-6.99.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'realurl' => '1.12.8-1.99.99',
+			'realurl' => '1.12.8-2.99.99',
 		),
 	),
 	'autoload' => array (
