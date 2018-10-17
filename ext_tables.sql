db00a028045a8971787cf983792d79a1e987d18c
Aimeos <aimeos@aimeos.org>
2018-10-17 16:56:42 +0200

    Remove automatically created cache tables from ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -16,25 +16,3 @@ CREATE TABLE fe_users (
     latitude decimal(8,6) DEFAULT NULL,
     vdate date DEFAULT NULL,
 );
-
-
-# Create cache tables for TYPO3 < 4.6
-
-CREATE TABLE tx_aimeos_cache (
-    id int(11) unsigned NOT NULL auto_increment,
-    identifier varchar(250) DEFAULT '' NOT NULL,
-    crdate int(11) unsigned DEFAULT '0' NOT NULL,
-    content mediumblob,
-    lifetime int(11) unsigned DEFAULT '0' NOT NULL,
-    PRIMARY KEY (id),
-    KEY cache_id (identifier)
-) ENGINE=InnoDB;
-
-CREATE TABLE tx_aimeos_cache_tags (
-    id int(11) unsigned NOT NULL auto_increment,
-    identifier varchar(250) DEFAULT '' NOT NULL,
-    tag varchar(250) DEFAULT '' NOT NULL,
-    PRIMARY KEY (id),
-    KEY cache_id (identifier),
-    KEY cache_tag (tag)
-) ENGINE=InnoDB;
