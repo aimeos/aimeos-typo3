ff98b87e83bd83af5fbcff63789308bca555ed34
Aimeos <aimeos@aimeos.org>
2018-12-05 15:01:29 +0100

    Use missing InputArgument class

--- Classes/Command/JobsCommand.php
+++ Classes/Command/JobsCommand.php
@@ -11,6 +11,7 @@ namespace Aimeos\Aimeos\Command;
 
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
