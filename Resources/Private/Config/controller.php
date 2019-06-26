4228295ff32f66fc4cd2a348e55be1bd62ceea36
Aimeos <aimeos@aimeos.org>
2019-06-26 11:10:57 +0200

    Removed old example config

--- Resources/Private/Config/controller.php
+++ Resources/Private/Config/controller.php
@@ -9,55 +9,8 @@ return array(
 				'mimeicon' => array(
 					// Directory where icons for the mime types stored Must be relative to the path in "basedir"
 					'directory' => '../../typo3conf/ext/aimeos/Resources/Public/Images/Mimeicons',
-
 					// File extension of mime type icons
-					// 'extension' => '.png',
-				),
-
-				// Parameter for images
-				'files' => array(
-					// Allowed image mime types, other image types will be converted
-					// 'allowedtypes' => array( 'image/jpeg', 'image/png', 'image/gif' ),
-
-					// Image type to which all other image types will be converted to
-					// 'defaulttype' => 'jpeg',
-
-					// Maximum width of an image
-					// Image will be scaled up or down to this size without changing the
-					// width/height ratio. A value of "null" doesn't scale the image or
-					// doesn't restrict the size of the image if it's scaled due to a value
-					// in the "maxheight" parameter
-					// 'maxwidth' => null,
-
-					// Maximum height of an image
-					// Image will be scaled up or down to this size without changing the
-					// width/height ratio. A value of "null" doesn't scale the image or
-					// doesn't restrict the size of the image if it's scaled due to a value
-					// in the "maxwidth" parameter
-					// 'maxheight' => null,
-				),
-
-				// Parameter for preview images
-				'preview' => array(
-					// Allowed image mime types, other image types will be converted
-					// 'allowedtypes' => array( 'image/jpeg', 'image/png', 'image/gif' ),
-
-					// Image type to which all other image types will be converted to
-					// 'defaulttype' => 'jpeg',
-
-					// Maximum width of a preview image
-					// Image will be scaled up or down to this size without changing the
-					// width/height ratio. A value of "null" doesn't scale the image or
-					// doesn't restrict the size of the image if it's scaled due to a value
-					// in the "maxheight" parameter
-					// 'maxwidth' => 360,
-
-					// Maximum height of a preview image
-					// Image will be scaled up or down to this size without changing the
-					// width/height ratio. A value of "null" doesn't scale the image or
-					// doesn't restrict the size of the image if it's scaled due to a value
-					// in the "maxwidth" parameter
-					// 'maxheight' => 280,
+					'extension' => '.png',
 				),
 			),
 		),
