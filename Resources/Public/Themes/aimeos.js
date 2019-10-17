0b1bb72cafe8605d235e74c252040f566dad1b79
Aimeos <aimeos@aimeos.org>
2019-10-17 09:54:26 +0200

    Update from core

--- Resources/Public/Themes/aimeos.js
+++ Resources/Public/Themes/aimeos.js
@@ -1232,10 +1232,41 @@ AimeosCatalogList = {
 
 
 	/**
+	 * Enables infinite scroll if available
+	 */
+	setupInfiniteScroll: function() {
+		if( $('.catalog-list-items').data('infinite-url') != '') {
+			$(window).on( 'scroll', function() {
+				var infiniteUrl = $('.catalog-list-items').data('infinite-url');
+				
+				if( infiniteUrl != '' && $('.catalog-list-items')[0].getBoundingClientRect().bottom - $(window).height() < 50 ) {
+					$('.catalog-list-items').data('infinite-url', '');
+					
+					$.ajax({
+						url: infiniteUrl
+					}).done( function( response ) {             
+						var nextPage = $( response );
+						nextPage.find('.catalog-list-items ul li').each( function() {
+							$('.catalog-list-items ul').append(this);
+						});
+						var nextUrl = nextPage.find('.catalog-list-items').data( 'infinite-url' );
+						$('.catalog-list-items').data('infinite-url', nextUrl);
+						$(window).trigger('scroll');
+					}).fail( function() {
+						$('.catalog-list-items').data('infinite-url', infiniteUrl);
+					});
+				}
+			});
+		}
+	},
+
+
+	/**
 	 * Initializes the catalog list actions
 	 */
 	init: function() {
 		this.setupImageSwitch();
+		this.setupInfiniteScroll();
 	}
 };
 
