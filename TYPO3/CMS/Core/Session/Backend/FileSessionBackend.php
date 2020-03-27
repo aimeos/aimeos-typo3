ff079c98851c9960390c05b5aa92911dca6ceec9
Aimeos <aimeos@aimeos.org>
2020-03-27 19:30:16 +0100

    Implements a file based session backend

---
+++ TYPO3/CMS/Core/Session/Backend/FileSessionBackend.php
@@ -0,0 +1,52 @@
+<?php
+
+class FileSessionBackend extends SessionBackendInterface
+{
+	public function initialize(string $identifier, array $configuration)
+	{
+	}
+
+
+	public function validateConfiguration()
+	{
+		return true;
+	}
+
+
+	public function getAll(): array
+	{
+		return $_SESSION;
+	}
+
+
+	public function get(string $sessionId): array
+	{
+		return (array) (isset( $_SESSION[$sessionId] ) ? $_SESSION[$sessionId] : []);
+	}
+
+
+	public function remove(string $sessionId): bool
+	{
+		unset($_SESSION[$sessionId]);
+		return true;
+	}
+
+
+	public function set(string $sessionId, array $sessionData): array
+	{
+		$_SESSION[$sessionId] = $sessionData;
+		return $sessionData;
+	}
+
+
+	public function update(string $sessionId, array $sessionData): array
+	{
+		$_SESSION[$sessionId] = $sessionData;
+		return $sessionData;
+	}
+
+
+	public function collectGarbage(int $maximumLifetime, int $maximumAnonymousLifetime = 0)
+	{
+	}
+}
