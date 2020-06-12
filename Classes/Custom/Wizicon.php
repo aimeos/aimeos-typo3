2106b7bcbb5ec0a4282e092bae2d659f397242c7
Aimeos <aimeos@aimeos.org>
2020-06-12 16:52:57 +0200

    Replace deprecated siteRelPath() method

--- Classes/Custom/Wizicon.php
+++ Classes/Custom/Wizicon.php
@@ -3,13 +3,14 @@
 /**
  * @license GPLv3, http://www.gnu.org/copyleft/gpl.html
  * @copyright Metaways Infosystems GmbH, 2013
- * @copyright Aimeos (aimeos.org), 2014-2016
+ * @copyright Aimeos (aimeos.org), 2014-2020
  * @package TYPO3
  */
 
 
 namespace Aimeos\Aimeos\Custom;
 
+use TYPO3\CMS\Core\Utility\PathUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Localization\LocalizationFactory;
@@ -31,8 +32,8 @@ class Wizicon
 	public function proc( $wizardItems )
 	{
 		$path = ExtensionManagementUtility::extPath( 'aimeos' );
-		$relpath = ExtensionManagementUtility::siteRelPath( 'aimeos' );
 		$langfile = $path . 'Resources/Private/Language/extension.xlf';
+		$relpath = PathUtility::stripPathSitePrefix( $path );
 
 		$languageFactory = GeneralUtility::makeInstance( 'TYPO3\CMS\Core\Localization\LocalizationFactory' );
 		$xml = $languageFactory->getParsedData( $langfile, $GLOBALS['LANG']->lang, '', 0 );
