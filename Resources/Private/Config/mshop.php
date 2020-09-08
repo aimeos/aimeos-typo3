f7f9927b22cae25f6fa3cf2ad2a2def4ff03a13a
Aimeos <aimeos@aimeos.org>
2020-09-08 12:48:20 +0200

    Simplified index manager configuration

--- Resources/Private/Config/mshop.php
+++ Resources/Private/Config/mshop.php
@@ -3,18 +3,7 @@
 return array(
 	'index' => array(
 		'manager' => array(
-			'attribute' => array(
-				'name' => 'MySQL',
-			),
-			'catalog' => array(
-				'name' => 'MySQL',
-			),
-			'price' => array(
-				'name' => 'MySQL',
-			),
-			'text' => array(
-				'name' => 'MySQL',
-			),
+			'name' => 'MySQL',
 		),
 	),
 	'customer' => array(
