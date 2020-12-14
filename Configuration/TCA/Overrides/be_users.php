8d685873f1a125d8a47abfca73afd97bdf7947a7
Aimeos <aimeos@aimeos.org>
2020-12-14 21:54:57 +0100

    Adapt coding style

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -1,62 +1,64 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
+if( !defined( 'TYPO3_MODE' ) ) {
+	die( 'Access denied.' );
 }
 
-$beUsersSiteFcn = function () {
-    $table = 'mshop_locale_site';
-    $tableConnection = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Database\ConnectionPool::class
-    )->getConnectionForTable($table);
 
-    $queryBuilder = $tableConnection->createQueryBuilder();
-    $list = [['', '']];
+$beUsersSiteFcn = function() {
 
-    if (empty($tableConnection->getSchemaManager()->listTableColumns($table)) !== true) {
-        $result = $queryBuilder->select('siteid', 'label', 'nleft', 'nright')
-            ->from($table)
-            ->orderBy('nleft')
-            ->execute();
+	$list = [['', '']];
+	$table = 'mshop_locale_site';
 
-        $parents = [];
+	$conn = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance( \TYPO3\CMS\Core\Database\ConnectionPool::class )
+		->getConnectionForTable( $table );
 
-        $fcn = function ($result, $parents, $right) use (&$fcn, &$list) {
-            while ($row = $result->fetch()) {
-                $list[] = [join(' > ', array_merge($parents, [$row['label']])), $row['siteid']];
+	if( !empty( $conn->getSchemaManager()->listTableColumns( $table ) ) )
+	{
+		$result = $conn->createQueryBuilder()
+			->select( 'siteid', 'label', 'nleft', 'nright' )
+			->from( $table )->orderBy( 'nleft' )
+			->execute();
 
-                if ($row['nright'] - $row['nleft'] > 1) {
-                    $fcn($result, array_merge($parents, [$row['label']]), $row['nright']);
-                }
+		$parents = [];
 
-                if ($row['nright'] + 1 == $right) {
-                    return;
-                }
-            }
-        };
+		$fcn = function( $result, $parents, $right ) use ( &$fcn, &$list ) {
 
-        while ($row = $result->fetch()) {
-            $list[] = [$row['label'], $row['siteid']];
+			while( $row = $result->fetch() ) {
+				$list[] = [join( ' > ', array_merge( $parents, [$row['label']] ) ), $row['siteid']];
 
-            if ($row['nright'] - $row['nleft'] > 1) {
-                $fcn($result, array_merge($parents, [$row['label']]), $row['nright']);
-            }
-        }
-    }
+				if( $row['nright'] - $row['nleft'] > 1 ) {
+					$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
+				}
 
-    return $list;
+				if( $row['nright'] + 1 == $right ) {
+					return;
+				}
+			}
+		};
+
+		while( $row = $result->fetch() ) {
+			$list[] = [$row['label'], $row['siteid']];
+
+			if( $row['nright'] - $row['nleft'] > 1 ) {
+				$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
+			}
+		}
+	}
+
+	return $list;
 };
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', [
-    'siteid' => [
-        'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
-            'items' => $beUsersSiteFcn(),
-        ]
-    ]
-]);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns( 'be_users', [
+	'siteid' => [
+		'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectSingle',
+			'items' => $beUsersSiteFcn(),
+		]
+	]
+] );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_users', 'siteid', '', 'after:password');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes( 'be_users', 'siteid', '', 'after:password' );
 
