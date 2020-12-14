de65511413984f9ae04162174fced235c4e7bfb2
stephankellermayr <stephan.kellermayr@gmail.com>
2020-12-14 19:23:17 +0100

    Replaced database query for TCA-items with createQueryBuilder()

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -1,69 +1,62 @@
 <?php
 
-if( !defined( 'TYPO3_MODE' ) ) {
-	die ( 'Access denied.' );
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
 }
 
+$beUsersSiteFcn = function () {
+    $table = 'mshop_locale_site';
+    $tableConnection = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Database\ConnectionPool::class
+    )->getConnectionForTable($table);
 
-$beUsersSiteFcn = function() {
+    $queryBuilder = $tableConnection->createQueryBuilder();
+    $list = [['', '']];
 
-	$table = 'mshop_locale_site';
-	$tableColumns = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)
-		->getConnectionForTable($table)
-		->getSchemaManager()
-		->listTableColumns($table);
+    if (empty($tableConnection->getSchemaManager()->listTableColumns($table)) !== true) {
+        $result = $queryBuilder->select('siteid', 'label', 'nleft', 'nright')
+            ->from($table)
+            ->orderBy('nleft')
+            ->execute();
 
-	$list = [['', '']];
-	if (empty($tableColumns) !== true) {
-		$sql = 'SELECT "siteid", "label", "nleft", "nright" FROM "' . $table . '" ORDER BY "nleft"';
-		$dbm = \Aimeos\Aimeos\Base::getContext( \Aimeos\Aimeos\Base::getConfig() )->db();
+        $parents = [];
 
-		$conn = $dbm->acquire( 'db-locale' );
-		$result = $conn->create( $sql )->execute();
+        $fcn = function ($result, $parents, $right) use (&$fcn, &$list) {
+            while ($row = $result->fetch()) {
+                $list[] = [join(' > ', array_merge($parents, [$row['label']])), $row['siteid']];
 
-		$parents = [];
+                if ($row['nright'] - $row['nleft'] > 1) {
+                    $fcn($result, array_merge($parents, [$row['label']]), $row['nright']);
+                }
 
-		$fcn = function( $result, $parents, $right ) use ( &$fcn, &$list ) {
+                if ($row['nright'] + 1 == $right) {
+                    return;
+                }
+            }
+        };
 
-			while( $row = $result->fetch() )
-			{
-				$list[] = [join( ' > ', array_merge( $parents, [$row['label']] ) ), $row['siteid']];
+        while ($row = $result->fetch()) {
+            $list[] = [$row['label'], $row['siteid']];
 
-				if( $row['nright'] - $row['nleft'] > 1 ) {
-					$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
-				}
+            if ($row['nright'] - $row['nleft'] > 1) {
+                $fcn($result, array_merge($parents, [$row['label']]), $row['nright']);
+            }
+        }
+    }
 
-				if( $row['nright'] + 1 == $right ) {
-					return;
-				}
-			}
-		};
-
-		while( $row = $result->fetch() )
-		{
-			$list[] = [$row['label'], $row['siteid']];
-
-			if( $row['nright'] - $row['nleft'] > 1 ) {
-				$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
-			}
-		}
-
-		$dbm->release( $conn, 'db-locale' );
-	}
-
-	return $list;
+    return $list;
 };
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', [
+    'siteid' => [
+        'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
+        'config' => [
+            'type' => 'select',
+            'renderType' => 'selectSingle',
+            'items' => $beUsersSiteFcn(),
+        ]
+    ]
+]);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns( 'be_users', [
-	'siteid' => [
-		'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
-		'config' => [
-			'type' => 'select',
-			'renderType' => 'selectSingle',
-			'items' => $beUsersSiteFcn(),
-		]
-	]
-] );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_users', 'siteid', '', 'after:password');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes( 'be_users', 'siteid', '', 'after:password' );
