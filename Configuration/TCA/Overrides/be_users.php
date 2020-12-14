46de47c43823aeeb6915175f2fe0fd5643d02378
Stephan Kellermayr <stephankellermayr@users.noreply.github.com>
2020-12-14 21:44:31 +0100

    Check existence of table 'mshop_locale_site' before adding items to TCA (#153)
    * Check existence of table 'mshop_locale_site' before adding items to TCA
    
    * Replaced database query for TCA-items with createQueryBuilder()

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -1,61 +1,64 @@
 <?php
 
 if( !defined( 'TYPO3_MODE' ) ) {
-	die ( 'Access denied.' );
+	die( 'Access denied.' );
 }
 
 
 $beUsersSiteFcn = function() {
 
-	$sql = 'SELECT "siteid", "label", "nleft", "nright" FROM "mshop_locale_site" ORDER BY "nleft"';
-	$dbm = \Aimeos\Aimeos\Base::getContext( \Aimeos\Aimeos\Base::getConfig() )->db();
-
-	$conn = $dbm->acquire( 'db-locale' );
-	$result = $conn->create( $sql )->execute();
-
-	$parents = [];
 	$list = [['', '']];
+	$table = 'mshop_locale_site';
 
-	$fcn = function( $result, $parents, $right ) use ( &$fcn, &$list ) {
+	$conn = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance( \TYPO3\CMS\Core\Database\ConnectionPool::class )
+		->getConnectionForTable( $table );
 
-		while( $row = $result->fetch() )
-		{
-			$list[] = [join( ' > ', array_merge( $parents, [$row['label']] ) ), $row['siteid']];
+	if( !empty( $conn->getSchemaManager()->listTableColumns( $table ) ) )
+	{
+		$result = $conn->createQueryBuilder()
+			->select( 'siteid', 'label', 'nleft', 'nright' )
+			->from( $table )->orderBy( 'nleft' )
+			->execute();
+
+		$parents = [];
+
+		$fcn = function( $result, $parents, $right ) use ( &$fcn, &$list ) {
+
+			while( $row = $result->fetch() ) {
+				$list[] = [join( ' > ', array_merge( $parents, [$row['label']] ) ), $row['siteid']];
+
+				if( $row['nright'] - $row['nleft'] > 1 ) {
+					$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
+				}
+
+				if( $row['nright'] + 1 == $right ) {
+					return;
+				}
+			}
+		};
+
+		while( $row = $result->fetch() ) {
+			$list[] = [$row['label'], $row['siteid']];
 
 			if( $row['nright'] - $row['nleft'] > 1 ) {
 				$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
 			}
-
-			if( $row['nright'] + 1 == $right ) {
-				return;
-			}
-		}
-	};
-
-	while( $row = $result->fetch() )
-	{
-		$list[] = [$row['label'], $row['siteid']];
-
-		if( $row['nright'] - $row['nleft'] > 1 ) {
-			$fcn( $result, array_merge( $parents, [$row['label']] ), $row['nright'] );
 		}
 	}
-
-	$dbm->release( $conn, 'db-locale' );
 
 	return $list;
 };
 
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns( 'be_users', [
-    'siteid' => [
-        'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
-        'config' => [
-            'type' => 'select',
-            'renderType' => 'selectSingle',
+	'siteid' => [
+		'label' => 'LLL:EXT:aimeos/Resources/Private/Language/admin.xlf:be_users_site.title',
+		'config' => [
+			'type' => 'select',
+			'renderType' => 'selectSingle',
 			'items' => $beUsersSiteFcn(),
-        ]
-    ]
+		]
+	]
 ] );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes( 'be_users', 'siteid', '', 'after:password' );
+
