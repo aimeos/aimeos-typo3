64031a88ea843935485f68b505c6d868f652a579
Aimeos <aimeos@aimeos.org>
2021-05-11 11:15:10 +0200

    Added JQAdm controller and actions to allow link() in admin templates

--- Resources/Private/Config/admin.php
+++ Resources/Private/Config/admin.php
@@ -4,41 +4,57 @@ return array(
 	'jqadm' => array(
 		'url' => array(
 			'copy' => array(
+				'controller' => 'Jqadm',
+				'action' => 'copy',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'create' => array(
+				'controller' => 'Jqadm',
+				'action' => 'create',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'delete' => array(
+				'controller' => 'Jqadm',
+				'action' => 'delete',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'export' => array(
+				'controller' => 'Jqadm',
+				'action' => 'export',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'get' => array(
+				'controller' => 'Jqadm',
+				'action' => 'get',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'import' => array(
+				'controller' => 'Jqadm',
+				'action' => 'import',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'save' => array(
+				'controller' => 'Jqadm',
+				'action' => 'save',
 				'config' => array(
 					'BE' => true,
 				),
 			),
 			'search' => array(
+				'controller' => 'Jqadm',
+				'action' => 'search',
 				'config' => array(
 					'BE' => true,
 				),
