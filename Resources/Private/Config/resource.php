0d3228bcc388e434d6a04ed58b07fe8e4dc709be
Tobias Gülzow <tobias.guelzow@brainworxx.de>
2021-12-11 11:18:19 +0100

    Additional checks for php 8 array keys. (#180)

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -2,19 +2,20 @@
 
 $publicPath = \TYPO3\CMS\Core\Core\Environment::getPublicPath();
 
+$defaultConnection = $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'];
 return [
 	'db' => [
 		'adapter' => 'mysql',
-		'host' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['host'],
-		'port' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['port'],
-		'socket' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['socket'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'],
-		'database' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['database'],
-		'username' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['username'],
-		'password' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] ?? $GLOBALS['TYPO3_CONF_VARS']['DB']['password'],
+		'host' => $defaultConnection['host'] ?? null,
+		'port' => $defaultConnection['port'] ?? null,
+		'socket' => $defaultConnection['socket'] ?? null,
+		'database' => $defaultConnection['dbname'] ?? null,
+		'username' => $defaultConnection['user'] ?? null,
+		'password' => $defaultConnection['password'] ?? null,
 		'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8'; SET SESSION sql_mode='ANSI'; SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED"],
 		'defaultTableOptions' => [
-			'charset' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['tableoptions']['charset'] ?? 'utf8',
-			'collate' => $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['tableoptions']['collate'] ?? 'utf8_unicode_ci',
+			'charset' => $defaultConnection['tableoptions']['charset'] ?? 'utf8',
+			'collate' => $defaultConnection['tableoptions']['collate'] ?? 'utf8_unicode_ci',
 		],
 		'limit' => 3,
 	],
