e98a95d3d3357c790d5d3253ef5e55c502cfc3fc
Aimeos <aimeos@aimeos.org>
2022-04-21 08:43:01 +0200

    Removed old configuration

--- Resources/Private/Config/controller.php
+++ Resources/Private/Config/controller.php
@@ -5,8 +5,6 @@ return array(
 		'media' => array(
 			// Mime icon related settings
 			'mimeicon' => array(
-				// Directory where icons for the mime types stored Must be relative to the path in "basedir"
-				'directory' => '../../typo3conf/ext/aimeos/Resources/Public/Images/Mimeicons',
 				// File extension of mime type icons
 				'extension' => '.png',
 			),
