e8747b0d6374acd72a7e73a5eccc11073e9e6f6a
Aimeos <aimeos@aimeos.org>
2023-02-02 10:54:42 +0100

    Remove explicit configuration of index sub-managers

--- Resources/Private/Config/mshop.php
+++ Resources/Private/Config/mshop.php
@@ -4,35 +4,11 @@ return [
 	'customer' => [
 		'manager' => [
 			'name' => 'Typo3',
-			'address' => [
-				'name' => 'Typo3',
-			],
-			'lists' => [
-				'name' => 'Typo3',
-			],
-			'property' => [
-				'name' => 'Typo3',
-			],
 		],
 	],
 	'index' => [
 		'manager' => [
 			'name' => 'MySQL',
-			'attribute' => [
-				'name' => 'MySQL',
-			],
-			'catalog' => [
-				'name' => 'MySQL',
-			],
-			'price' => [
-				'name' => 'MySQL',
-			],
-			'supplier' => [
-				'name' => 'MySQL',
-			],
-			'text' => [
-				'name' => 'MySQL',
-			],
 		],
 	],
 ];
