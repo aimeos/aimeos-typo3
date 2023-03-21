6cc8e0d557226aaadc7cb000382336edbc83fc65
Ralf Habacker <ralf.habacker@freenet.de>
2023-03-21 19:49:26 +0100

    Fixes for using postgresql database (#201)

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -2,17 +2,33 @@
 
 $publicPath = \TYPO3\CMS\Core\Core\Environment::getPublicPath();
 $defaultConnection = $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'];
+$driver = $defaultConnection['driver'];
+
+switch($driver) {
+	case 'mysqli':
+	case 'pdo_mysql': $adapter = 'mysql'; break;
+	case 'pdo_oci': $adapter = 'oracle'; break;
+	case 'pdo_pgsql': $adapter = 'pgsql'; break;
+	case 'pdo_sqlite': $adapter = 'sqlite'; break;
+	case 'pdo_sqlsrv': $adapter = 'sqlsrv'; break;
+	default: $adapter = $driver;
+}
+
+switch($adapter) {
+	case 'mysql': $stmt = ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8'; SET SESSION sql_mode='ANSI'; SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED"]; break;
+	default: $stmt = [];
+}
 
 return [
 	'db' => [
-		'adapter' => ($defaultConnection['driver'] ?? 'mysql') !== 'mysqli' ?: 'mysql',
+		'adapter' => $adapter,
 		'host' => $defaultConnection['host'] ?? null,
 		'port' => $defaultConnection['port'] ?? null,
 		'socket' => $defaultConnection['socket'] ?? null,
 		'database' => $defaultConnection['dbname'] ?? null,
 		'username' => $defaultConnection['user'] ?? null,
 		'password' => $defaultConnection['password'] ?? null,
-		'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8'; SET SESSION sql_mode='ANSI'; SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED"],
+		'stmt' => $stmt,
 		'defaultTableOptions' => [
 			'charset' => $defaultConnection['tableoptions']['charset'] ?? 'utf8',
 			'collate' => $defaultConnection['tableoptions']['collate'] ?? 'utf8_unicode_ci',
