ecd8a67af88b43bf5e161f27ee2e49a0837a5e56
Ralf Habacker <ralf.habacker@freenet.de>
2023-03-22 16:51:30 +0100

    Support databases other than MySQL for index manager (#202)

--- Resources/Private/Config/mshop.php
+++ Resources/Private/Config/mshop.php
@@ -1,5 +1,19 @@
 <?php
 
+$defaultConnection = $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] ?? [];
+$driver = $defaultConnection['driver'] ?? null;
+
+switch($driver) {
+	case 'mysql':
+	case 'mysqli':
+	case 'pdo_mysql': $manager = 'MySQL'; break;
+	case 'pgsql':
+	case 'pdo_pgsql': $manager = 'PgSQL'; break;
+	case 'sqlsrv':
+	case 'pdo_sqlsrv': $manager = 'SQLSrv'; break;
+	default: $manager = 'Standard';
+}
+
 return [
 	'customer' => [
 		'manager' => [
@@ -8,7 +22,22 @@ return [
 	],
 	'index' => [
 		'manager' => [
-			'name' => 'MySQL',
+			'name' => $manager,
+			'attribute' => [
+				'name' => $manager,
+			],
+			'catalog' => [
+				'name' => $manager,
+			],
+			'price' => [
+				'name' => $manager,
+			],
+			'supplier' => [
+				'name' => $manager,
+			],
+			'text' => [
+				'name' => $manager,
+			],
 		],
 	],
 ];
