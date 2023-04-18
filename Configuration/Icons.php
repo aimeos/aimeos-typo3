9783c08f6b8fa4828660f5075224529a8e3a500f
Aimeos <aimeos@aimeos.org>
2023-04-18 11:33:06 +0200

    Added icons

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'aimeos-shop' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:aimeos/Resources/Public/Icons/Extension.svg'
+    ],
+];
