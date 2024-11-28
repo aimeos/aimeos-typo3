11943a17da08fd7549565c11ee1931498446a22f
Aimeos <aimeos@aimeos.org>
2024-11-28 11:49:00 +0100

    Update from core

--- Resources/Public/Themes/default/basket-standard.js
+++ Resources/Public/Themes/default/basket-standard.js
@@ -26,8 +26,9 @@ AimeosBasketStandard = {
 
 		$(document).on("submit", ".basket-standard form", ev => {
 			Aimeos.createSpinner();
+			const selector = ev.submitter || ev.originalEvent.submitter;
 
-			fetch($(ev.submitter).attr("formaction") || $(ev.currentTarget).attr("action"), {
+			fetch($(selector).attr("formaction") || $(ev.currentTarget).attr("action"), {
 				body: new FormData(ev.currentTarget),
 				method: 'POST'
 			}).then(response => {
