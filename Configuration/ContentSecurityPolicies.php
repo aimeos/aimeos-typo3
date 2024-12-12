254dce65a9675e99fb970812c166a7bdf5f3426a
Aimeos <aimeos@aimeos.org>
2024-12-12 11:35:46 +0100

    Updated backend security policy

--- Configuration/ContentSecurityPolicies.php
+++ Configuration/ContentSecurityPolicies.php
@@ -16,25 +16,22 @@ return Map::fromEntries([
     Scope::backend(),
 
     new MutationCollection(
-        // Results in `default-src 'self' 'data:', 'blob:' 'https://cdnjs.cloudflare.com' 'https://cdn.jsdelivr.net'`
+        // Results in `default-src 'self' 'data:', 'blob:'`
         new Mutation(
             MutationMode::Extend,
             Directive::DefaultSrc,
             SourceKeyword::self,
             SourceScheme::data,
-            SourceScheme::blob,
-            new UriValue('https://cdnjs.cloudflare.com'),
-            new UriValue('https://cdn.jsdelivr.net')
+            SourceScheme::blob
         ),
 
-        // Results in `style-src 'self' 'unsafe-inline' 'https://cdnjs.cloudflare.com' 'https://cdn.jsdelivr.net'`
+        // Results in `style-src 'self' 'unsafe-inline' 'https://cdnjs.cloudflare.com'`
         new Mutation(
             MutationMode::Extend,
             Directive::StyleSrc,
             SourceKeyword::self,
             SourceKeyword::unsafeInline,
-            new UriValue('https://cdnjs.cloudflare.com'),
-            new UriValue('https://cdn.jsdelivr.net')
+            new UriValue('https://cdnjs.cloudflare.com')
         ),
 
         // Results in `script-src 'self' 'unsafe-eval' 'https://cdnjs.cloudflare.com' 'https://cdn.jsdelivr.net'`
@@ -42,10 +39,7 @@ return Map::fromEntries([
             MutationMode::Extend,
             Directive::ScriptSrc,
             SourceKeyword::self,
-            SourceKeyword::unsafeEval,
-            new UriValue('https://cdnjs.cloudflare.com'),
-            new UriValue('https://cdn.jsdelivr.net'),
-            new UriValue('https://cdn.jsdelivr.net')
+            SourceKeyword::unsafeEval
         ),
 
         // Results in `connect-src 'self' https://*.deepl.com https://api.openai.com`
@@ -64,8 +58,6 @@ return Map::fromEntries([
             SourceKeyword::self,
             SourceScheme::data,
             SourceScheme::blob,
-            new UriValue('https://cdnjs.cloudflare.com'),
-            new UriValue('https://cdn.jsdelivr.net'),
             new UriValue('https://*.tile.openstreetmap.org'),
             new UriValue('https://aimeos.org')
         ),
